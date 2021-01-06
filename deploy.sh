3981cc6369bc7a861822af27da57fae5953b70b1
Itchakhun <16811182+itchakhun@users.noreply.github.com>
2021-01-07 03:20:08 +0700

    🔨 deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+yarn run build
+
+# navigate into the build output directory
+cd dist
+
+git init
+
+git add -A
+
+git commit -m 'deploy'
+
+git push -f https://github.com/itchakhun/crispy-eureka.git main:gh-pages
+
+cd -
